95186f695b8610c929e04055c8fe582226094dee
corinthdesign <tim@corinthdesign.com>
2025-06-24 14:56:15 -0400

    Updated app styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,5 @@
-import React from 'react';
-
-// src/pages/Login.js
+import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient.js';
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
@@ -37,40 +34,38 @@ function Login() {
     if (error) {
       setMessage('Error: ' + error.message);
     } else {
-      setMessage('Check your email for the login link!');
+      setMessage('Check your email for the magic login link! ✨');
     }
   };
-  
 
   return (
-        <div id="welcome">
-        <div id="greeting">
-          <h1>❤️</h1>
-          <h1>Welcome to Couple Sync</h1>
-          <h3>An app created to keep your marriage in sync ♾️</h3>
-        </div>
-        <div id="intake">
-          <form onSubmit={handleLogin}>
-            <input
-              id="email"
-              name="email"
-              type="email"
-              placeholder="What's Your Email?"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="border p-2 mr-2"
-              required
-            />
-            <button type="submit" className="button secondary">
-              Send Magic Link
-            </button>
-      </form>
-      {message && <p className="mt-4">{message}</p>}
-        </div>
+    <div className="page-container" id="welcome">
+      <div id="greeting">
+        <h1>❤️</h1>
+        <h1>Welcome to Couple Sync</h1>
+        <h3>An app created to keep your marriage in sync ♾️</h3>
       </div>
+
+      <div id="intake">
+        <form onSubmit={handleLogin}>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="What's Your Email?"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <button type="submit" className="button secondary">
+            Send Magic Link
+          </button>
+        </form>
+
+        {message && <p className="mt-4">{message}</p>}
+      </div>
+    </div>
   );
 }
 
-
 export default Login;
-
